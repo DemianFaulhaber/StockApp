ccba4bd5750b787e5937fc0e233bf94e73d364ca
unknown <demiansilver22@gmail.com>
2023-06-22 14:50:05 -0300

    medidas de seguridad añadidas

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -24,6 +24,19 @@ function search(req, res){
     });
 }
 
+function nueva_venta(req, res){
+    const codigo = req.query.codigo;
+    console.log(codigo)
+    req.getConnection((err,conn) => {
+        conn.query("SELECT * FROM productos WHERE codigo = ?", [codigo], (err, productos) => {
+            if(err){
+                res.json(err);
+            }
+            res.render('tasks/nueva', {productos});
+        });
+    });
+}
+
 function agregar(req, res){
     res.render('tasks/agregar');
 }
@@ -34,14 +47,28 @@ function venta(req, res){
 
 function store(req, res){
     const data = req.body;
-
-    req.getConnection((err, conn) => {
-        conn.query('INSERT INTO productos SET ?', [data], (err, rows) =>{
-            console.log(rows);
-        })
+    req.getConnection((err,conn) => {
+        const codigo = req.body.codigo;
+        conn.query("SELECT codigo FROM productos WHERE codigo = ?", [codigo], (err, productos) => {
+            if(productos.length != 0){
+                res.render('tasks/agregar', {productos})
+            }
+            else{
+                const messages = {
+                    message:'Completar campos faltantes'
+                }
+                req.getConnection((err, conn) => {
+                    conn.query('INSERT INTO productos SET ?', [data], (err, rows) =>{
+                        if(rows == undefined){
+                            console.log(messages.message)
+                            res.render('tasks/agregar', {messages})
+                        }
+                    })
+                })
+            }
+        }); 
     })
 }
-
 
 module.exports = {
     index: index,
@@ -49,5 +76,6 @@ module.exports = {
     venta: venta,
     store: store,
     agregar: agregar,
+    nueva_venta: nueva_venta,
 }
 
